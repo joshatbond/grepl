d311959b38e33d2c7bfef5c12e66a4f7563db830
Joshua Richardson <josh@cursedtale.com>
2024-02-02 13:40:01 -0700

    add title board

---
+++ src/app/_components/TitleBoard.tsx
@@ -0,0 +1,28 @@
+import { cx } from "class-variance-authority";
+import styles from "~/styles/titleBoard.module.css";
+
+export default function TitleBoard() {
+  return (
+    <div className={styles.board}>
+      <Cubes />
+      <div className={styles.base}></div>
+    </div>
+  );
+}
+
+function Cubes() {
+  const cubes = "gqvxkrwtyewlbopm".split("");
+  const getColor = (n: number) =>
+    [0, 5, 9, 11, 14].includes(n) ? styles.cubeActive : "";
+  const getDelay = (n: number) => styles[`delay-${(n + 1) * 100}`] ?? "";
+
+  return (
+    <>
+      {cubes.map((e, i) => (
+        <div key={i} className={cx(styles.cube, getColor(i), getDelay(i))}>
+          {e}
+        </div>
+      ))}
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import TitleBoard from "./_components/TitleBoard";
+
 export default function HomePage() {
   return (
-    <main className="overflow-auto">
-      <p>Here</p>
+    <main className="wrapper gap-y-12">
+      <div className="wrapper__full-bleed">
+        <TitleBoard />
+      </div>
     </main>
   );
 }
