946c753f721539a82a7c5c9ef2d50867b21cb650
Joshua Richardson <josh@cursedtale.com>
2024-02-02 13:39:24 -0700

    setup theming

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,10 +5,18 @@ export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
+      backgroundColor: {
+        default: "var(--clr-bg)",
+        accent: "var(--clr-bg-accent)",
+      },
       fontFamily: {
         amatic: ["var(--font-amatic)", ...fontFamily.sans],
         roboto: ["var(--font-roboto)", ...fontFamily.sans],
         sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+      textColor: {
+        primary: "var(--clr-text-primary)",
+        inverted: "var(--clr-text-inverted)",
       },
     },
   },
