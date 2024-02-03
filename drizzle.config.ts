d0b78b691beef5865cb806d51df5185567bf3ea2
Joshua Richardson <josh@cursedtale.com>
2024-02-02 17:33:15 -0700

    further remove drizzle until db is set up

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,12 @@
-import { type Config } from "drizzle-kit";
+// import { type Config } from "drizzle-kit";
 
-import { env } from "~/env.js";
+// import { env } from "~/env.js";
 
-export default {
-  schema: "./src/server/db/schema.ts",
-  driver: "better-sqlite",
-  dbCredentials: {
-    url: env.DATABASE_URL,
-  },
-  tablesFilter: ["grepl_*"],
-} satisfies Config;
+// export default {
+//   schema: "./src/server/db/schema.ts",
+//   driver: "better-sqlite",
+//   dbCredentials: {
+//     url: env.DATABASE_URL,
+//   },
+//   tablesFilter: ["grepl_*"],
+// } satisfies Config;
