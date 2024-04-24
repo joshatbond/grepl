5f477084bd3d58cd2add1525e7124cfd71e88cd8
Joshua Richardson <jrichardson@wearebond.com>
2024-04-23 20:36:34 -0600

    fix(db): Add schema to db

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -3,6 +3,9 @@ import { drizzle } from 'drizzle-orm/libsql'
 
 import { env } from '~/env.js'
 
+import * as schema from './schema'
+
 export const db = drizzle(
-  createClient({ url: env.DATABASE_URL, authToken: env.DATABASE_AUTH_TOKEN })
+  createClient({ url: env.DATABASE_URL, authToken: env.DATABASE_AUTH_TOKEN }),
+  { schema }
 )
